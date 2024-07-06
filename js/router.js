c2ba37e879806a139d33ce3dd24faa63a3ea41b6
Serhiy Morenko <serhiy.greench@gmail.com>
2024-07-06 15:41:50 +0300

    wip: routing

--- js/router.js
+++ js/router.js
@@ -9,13 +9,14 @@ const handleLocation = async () => {
   const path = window.location.pathname;
   const params = path.split('/').filter(i => i);
 
-  console.log(path);
+  const url = new URL(window.location.href);
+
+  const postId = url.searchParams.get('post');
+
+  console.log(postId);
 
   const firstParam = params[0];
   const secondParam = params[1];
-
-  console.log(path);
-  console.log(window.applicationOperations.openPost.toString());
 
   if (firstParam === 'post') {
     window.applicationOperations.openPost(secondParam);
